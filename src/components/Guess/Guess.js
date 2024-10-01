a4ee0b0bbaf0c2c8c6aa4b711b045446c44c989c
krisnadibyo <krisna_itbstei@hotmail.com>
2024-10-01 13:05:18 +0700

    format doc

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import { range } from '../../utils';
+import React from "react";
+import { range } from "../../utils";
 
-
-function Guess({checkLetter}) {
-  return  (
-  <p className="guess">
-    {range(0, 5, 1).map((i) => <span className={`cell ${checkLetter && checkLetter[i].status}`} key={crypto.randomUUID()}>{checkLetter && checkLetter[i].letter}</span>)}
-  </p>
-);
+function Guess({ checkLetter }) {
+  return (
+    <p className="guess">
+      {range(0, 5, 1).map((i) => (
+        <span
+          className={`cell ${checkLetter && checkLetter[i].status}`}
+          key={crypto.randomUUID()}
+        >
+          {checkLetter && checkLetter[i].letter}
+        </span>
+      ))}
+    </p>
+  );
 }
 
 export default Guess;
--- src/components/GuessForm/GuessForm.js
+++ src/components/GuessForm/GuessForm.js
@@ -1,34 +1,37 @@
-import React from 'react';
+import React from "react";
 
-function GuessForm({handleSubmitForm}) {
-  const [guess, setGuess] = React.useState('')
+function GuessForm({ handleSubmitForm }) {
+  const [guess, setGuess] = React.useState("");
 
   const handleSubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     if (guess.length != 5) {
-      window.alert("WORD must be 5 length")
+      window.alert("WORD must be 5 length");
       return;
     }
-    console.log({guess})
-    handleSubmitForm(guess)
-    setGuess("")
-  }  
+    console.log({ guess });
+    handleSubmitForm(guess);
+    setGuess("");
+  };
   return (
-  <form className='guess-input-wrapper'
-    onSubmit={(event) => handleSubmit(event) }
-  >
-    <label htmlFor="guess-input">Enter Guess: </label>
-    <input id="guess-input" type='text' 
-      value={guess}
-      onChange={(event) => {
-        if (event.target.value.length > 5) {
-          return;
-        }
-        setGuess(event.target.value.toUpperCase())
-      
-      } }
-    />
-  </form>);
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(event) => handleSubmit(event)}
+    >
+      <label htmlFor="guess-input">Enter Guess: </label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={(event) => {
+          if (event.target.value.length > 5) {
+            return;
+          }
+          setGuess(event.target.value.toUpperCase());
+        }}
+      />
+    </form>
+  );
 }
 
 export default GuessForm;
